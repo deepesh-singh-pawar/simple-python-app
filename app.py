ededdb711592ae66c51a62d5a0834b4f3f398c64
devopsbatch23012025 <automationdevopsbatch@gmail.com>
2026-03-23 01:05:02 +0530

    clear code error spacing-coding

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 def home():
     return "Hello DevOps Engineer 🚀 Your app is running successfully!"
 
+
 @app.route("/health")
 def health():
     return {"status": "healthy"}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
